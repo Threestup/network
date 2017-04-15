54fed0f04f591f6690a4aa9b11cc4955144edbd1
Slavomir Vojacek <slavomirvojacek@gmail.com>
2017-04-15 13:09:31 +0100

    Added Fetch and FakeProvider exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export { ApiMethod, IApiConfig, IBaseApi, IApi, BaseApi, } from './Api/Api';
-export { IBaseNetwork, INetwork, IStubNetwork, BaseNetwork } from './Api/Network';
+export { IBaseNetwork, INetwork, IStubNetwork, BaseNetwork, Fetch, ConfigureFakeProvider } from './Api/Network';
